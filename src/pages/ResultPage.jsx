ac865479fe02309505b808800ac4b91bb09455c5
27Lia <117743861+27Lia@users.noreply.github.com>
2024-01-15 05:20:26 +0900

    feat:결과페이지 -> 메인 페이지이동 추가

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,11 +1,23 @@
-// 처음 화면 
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
 function ResultPage() {
-    return (
-      <div>
-        <p>결과 페이지</p>
-      </div>
-    );
-  }
-  
-  export default ResultPage;
-  
+  const location = useLocation();
+  const mbtiType = location.state?.mbtiType;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      {mbtiType ? (
+        <h1>당신의 MBTI 유형은 {mbtiType}입니다.</h1>
+      ) : (
+        <p>MBTI 유형이 제공되지 않았습니다.</p>
+      )}
+      <button className="answerbtn" onClick={() => navigate("/")}>
+        다시하기
+      </button>
+    </div>
+  );
+}
+
+export default ResultPage;
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,5 +1,5 @@
 import "../TestPage.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import questionData from "../data/questions.json";
 import React, { useState } from "react";
 
