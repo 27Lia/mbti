aa552605c426a5d2a0233158105db31091914a9a
27Lia <117743861+27Lia@users.noreply.github.com>
2024-01-15 08:13:54 +0900

    doc: json 데이터 수정

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -8,19 +8,10 @@ function ResultPage() {
   const navigate = useNavigate();
   // 사용자의 mbti 데이터를 가져옴
   const mbtiInfo = mbtiData[mbtiType];
+  console.log(mbtiInfo);
 
   // 사용자의 mbti 데이터에서 compatibleTypes을 추출
-  const compatibleMemberTypes = mbtiInfo.compatibleTypes;
-
-  // 추출한 compatibleMember를 mbtidata에서 찾아내어 추출
-  const compatibleMembers = [];
-
-  Object.keys(mbtiData).forEach((key) => {
-    if (compatibleMemberTypes.includes(key)) {
-      compatibleMembers.push(mbtiData[key]);
-    }
-  });
-  const compatibleMember = compatibleMembers[0].member[0].description;
+  const compatibleMember = mbtiInfo.compatibleTypes[0].description;
 
   return (
     <div>
@@ -28,8 +19,9 @@ function ResultPage() {
         <div>
           <h1>당신의 유형은 {mbtiInfo.member[0].member}입니다.</h1>
           <div>{mbtiInfo.member[0].description}</div>
-          <h2>당신과 어울리는 NCT 멤버는 다음과 같습니다:</h2>
-          {compatibleMember}
+          <h2>
+            당신과 어울리는 NCT 멤버는 다음과 같습니다: {compatibleMember}
+          </h2>
         </div>
       ) : (
         <p>MBTI 유형이 제공되지 않았습니다.</p>
