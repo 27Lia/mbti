b2b42684d9aaa9bcb171bbd4395f48fe2976be3c
Se Jin <154483251+S-e-Jin@users.noreply.github.com>
2024-01-23 00:23:06 +0900

    test: 모바일뷰포트 테스트

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import MainPage from "./pages/MainPage";
 import TestPage from "./pages/TestPage";
@@ -5,6 +6,21 @@ import ResultPage from "./pages/ResultPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    function setScreenSize() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+    setScreenSize(); 
+
+    // 창 크기가 변경될 때마다 setScreenSize 호출
+    window.addEventListener("resize", setScreenSize);
+
+    return () => {
+      window.removeEventListener("resize", setScreenSize);
+    };
+  }, []); 
+
   return (
     <div className="App">
       <Routes>
