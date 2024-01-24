ad1b30714bac28d6854e4ac81b0c9bc18afd64f2
27Lia <117743861+27Lia@users.noreply.github.com>
2024-01-24 21:56:17 +0900

    feat: 사진저장기능 추가

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import mbtiData from "../data/nctMembers.json";
 import "../styles/ResultPage.css";
 import LoadingPage from "./LoadingPage";
+import html2canvas from "html2canvas";
 
 function ResultPage() {
   const location = useLocation();
@@ -14,21 +15,30 @@ function ResultPage() {
   const baseUrl = "https://nctmbti.vercel.app";
   const memberImg = mbtiInfo.member[0].memberImg;
   const [isImgLoaded, setIsImgLoaded] = useState(false);
-  const [showLoading, setShowLoading] = useState(true);
+  const resultPageRef = useRef();
+
+  const handleSaveAsImage = async () => {
+    const element = resultPageRef.current; // 현재 페이지의 DOM 요소
+    const canvas = await html2canvas(element); // 해당 요소를 캔버스로 변환
+    const image = canvas.toDataURL("image/png"); // 캔버스를 이미지(PNG 형식)로 변환
+    const link = document.createElement("a"); // 새로운 링크(a 태그)를 생성
+    link.href = image; // 링크의 href 속성을 이미지 URL로 설정
+    link.download = "result-page.png"; // 다운로드될 때 기본 파일 이름 설정
+    document.body.appendChild(link); // 생성한 링크를 body에 추가
+    link.click(); // 링크를 클릭하면 이미지 다운로드 시작
+    document.body.removeChild(link); // 사용이 끝난 링크를 body에서 제거
+  };
 
   useEffect(() => {
-    // 이미지 로드 이벤트 리스너 등록
     const imgElement = new Image();
     imgElement.src = memberImg;
     imgElement.onload = () => {
       setIsImgLoaded(true);
-      // After image loaded, wait for 5 seconds and then hide the loading page
       setTimeout(() => {
-        setShowLoading(false);
-      }, 2000);
+        setIsImgLoaded(true);
+      }, 3000);
     };
 
-    // 컴포넌트 언마운트 시 이벤트 리스너 해제
     return () => {
       imgElement.onload = null;
     };
@@ -44,9 +54,9 @@ function ResultPage() {
   };
 
   return (
-    <div className="resultPage">
-      {showLoading && <LoadingPage />}
-      {!showLoading && mbtiType ? (
+    <div className="resultPage" ref={resultPageRef}>
+      {!isImgLoaded && <LoadingPage />}{" "}
+      {isImgLoaded && mbtiType ? (
         <div>
           <img
             key="loading-image"
@@ -81,15 +91,20 @@ function ResultPage() {
         <p>MBTI 유형이 제공되지 않았습니다.</p>
       )}
       <br />
-      <button
-        className="retry-btn"
-        onClick={() => handleCopyClipBoard(`${baseUrl}${location.pathname}`)}
-      >
-        공유링크
-      </button>
-      <button className="retry-btn" onClick={() => navigate("/")}>
-        다시하기
-      </button>
+      <div className="btn-box">
+        <button
+          className="retry-btn"
+          onClick={() => handleCopyClipBoard(`${baseUrl}${location.pathname}`)}
+        >
+          공유링크
+        </button>
+        <button className="retry-btn" onClick={handleSaveAsImage}>
+          저장
+        </button>
+        <button className="retry-btn" onClick={() => navigate("/")}>
+          다시하기
+        </button>
+      </div>
     </div>
   );
 }
