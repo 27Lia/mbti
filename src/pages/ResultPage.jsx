53c7bb09839c0c6052d28e5c5b312b6e92de111e
27Lia <117743861+27Lia@users.noreply.github.com>
2024-01-28 00:55:43 +0900

    fix: 공유시 주소값 추가

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -58,6 +58,7 @@ function ResultPage() {
         .share({
           title: "MBTI 결과 공유",
           text: "나와 NCT 멤버의 궁합 결과를 확인해보세요!",
+          url: currentUrl,
         })
         .then(() => console.log("공유 성공!"))
         .catch((error) => console.log("공유에 실패했습니다", error));
